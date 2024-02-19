2535fd3d3ec3a9b211d52018279f0791a97914ac
Anders <lynge1996@live.dk>
2024-02-19 19:53:22 +0100

    første commit

---
+++ Shared/Paths.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Shared
+{
+    public class Paths
+    {
+        public static string FOLDER = @"/Users/lynge/Desktop/Data/seData/small";
+
+        public static string DATABASE = @"/Users/lynge/Desktop/Data/docsearch.db";
+    }
+}
