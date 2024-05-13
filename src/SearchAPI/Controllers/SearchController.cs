0f8af5c9d217a7019a30d46f321f32fd498f3bad
JakaTrifork <jaka@trifork.com>
2024-05-13 15:07:30 +0200

    Added basic logic for modal view

--- src/SearchAPI/Controllers/SearchController.cs
+++ src/SearchAPI/Controllers/SearchController.cs
@@ -3,7 +3,7 @@ using Core.DTOs;
 using Core.Settings;
 using Microsoft.AspNetCore.Mvc;
 using SearchAPI.Interfaces;
-using Serilog;
+using System.IO;
 
 namespace SearchAPI.Controllers;
 
@@ -28,8 +28,9 @@ public class SearchController : ControllerBase
             var result = _searchLogic.Search(search, AdvancedSettings.SearchResults);
 
             var dto = SearchResultMapper.SearchResultToDTO(result);
-            
-            _logger.LogInformation("Search Results for query: '{search}' - {hits} hits in {timeElapsed:F2} ms", search, dto.Hits, dto.TimeUsed.TotalMilliseconds);
+
+            _logger.LogInformation("Search Results for query: '{search}' - {hits} hits in {timeElapsed:F2} ms", search,
+                dto.Hits, dto.TimeUsed.TotalMilliseconds);
 
             return Ok(dto);
         }
@@ -39,4 +40,26 @@ public class SearchController : ControllerBase
         }
     }
 
+    [HttpGet(Name = "GetFileContent")]
+    public ActionResult<string> GetFileContent([FromQuery] string fileName)
+    {
+        try
+        {
+            string filePath = _searchLogic.GetFilePath(fileName);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                string content = System.IO.File.ReadAllText(filePath);
+                return content;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Error while trying to get file content", e);
+        }
+    }
 }
